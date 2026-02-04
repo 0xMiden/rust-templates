2a290a3627eed02e4cf72c5ca1da34caa9a088a4
Denys Zadorozhnyi <denys@zadorozhnyi.com>
2026-02-04 16:11:54 +0200

    chore: update to SDK v0.9

--- note/template/src/lib.rs
+++ note/template/src/lib.rs
@@ -3,12 +3,20 @@
 
 extern crate alloc;
 
-use bindings::miden::add_contract::add_contract::add;
-use miden::{felt, note_script, Felt, Word};
+use miden::{felt, note, Word};
 
-#[note_script]
-fn run(_arg: Word) {
-    let a = felt!(1);
-    let b = felt!(2);
-    add(a, b);
+use bindings::miden::add_contract::add_contract::add;
+use bindings::Account;
+
+#[note]
+struct MyNote {}
+
+#[note]
+impl MyNote {
+    #[note_script]
+    pub fn run(self, _arg: Word, _account: &mut Account) {
+        let a = felt!(1);
+        let b = felt!(2);
+        add(a, b);
+    }
 }
